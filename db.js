37b0b969c58f67a522945b032ed6c85b71fbf406
Gabriel-L <arilgabriel4@gmail.com>
2024-05-13 21:33:31 -0400

    Done

---
+++ db.js
@@ -0,0 +1,28 @@
+"use strict";
+/** Database setup for jobly. */
+const { Client } = require("pg");
+const { getDatabaseUri } = require("./config");
+
+let db;
+
+if (process.env.NODE_ENV === "production") {
+  db = new Client({
+    connectionString: getDatabaseUri(),
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  });
+} else {
+  db = new Client({
+    connectionString: getDatabaseUri(),
+  });
+}
+
+db.connect((err) => {
+  if (err) {
+    console.log("failed to connect to db", err);
+  }
+  console.log("Successfully connect to the db");
+});
+
+module.exports = db;
